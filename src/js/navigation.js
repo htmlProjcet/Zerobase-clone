c7b059bafbed1ccd1a5952ee598a552d5adb509f
Eunseong, Yeo <e7838752@gmail.com>
2021-11-17 11:15:57 +0900

    ♻️ refator: Add toggle to button

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -7,9 +7,14 @@ for (let i = 0; i < listItems.length; i++) {
       listItems[activeIndex]
         .querySelector('.sub-menu')
         .classList.remove('is-active');
+      listItems[activeIndex]
+        .querySelector('.header-nav__button')
+        .classList.remove('is-active');
     }
     const subMenu = listItems[i].querySelector('.sub-menu');
+    const btn = listItems[i].querySelector('.header-nav__button');
     subMenu.classList.add('is-active');
+    btn.classList.add('is-active');
     activeIndex = i;
   });
 }
