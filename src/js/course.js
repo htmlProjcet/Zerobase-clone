6bba471884afa72198ca4997321b29e6d684b5c9
Eunseong, Yeo <e7838752@gmail.com>
2021-11-18 16:37:17 +0900

    ✏️ fix: slider bug fix

--- src/js/course.js
+++ src/js/course.js
@@ -1,25 +1,27 @@
-let scrollAmount = 0;
-let scrollMin = 0;
-const scrollMax = 248;
+const leftButton = document.querySelector('.button-practice-prev');
+const rightButton = document.querySelector('.button-practice-next');
+const cardContainer = document.querySelector('.practice-container');
+const card = document.querySelectorAll('.card-practice');
+const n = card.length;
 
-[0, 1, 2, 3].forEach((index) => {
-  const leftButton = document.querySelector('.button-practice-prev');
-  const rightButton = document.querySelector('.button-practice-next');
-  const cardContainer =
-    document.querySelector('.practice-container');
+let posLeft = Array(n);
+for (let i = 0; i < n; i++) {
+  posLeft[i] = card[i].offsetLeft;
+}
 
-
-  leftButton.addEventListener('click', () => {
-    cardContainer.scrollTo({
-      left: Math.min((scrollAmount -= 500), scrollMin),
-      behavior: 'smooth',
-    });
+let index = 0;
+leftButton.addEventListener('click', () => {
+  index = Math.max(index - 1, 0);
+  cardContainer.scrollTo({
+    left: posLeft[index],
+    behavior: 'smooth',
   });
+});
 
-  rightButton.addEventListener('click', () => {
-    cardContainer.scrollTo({
-      left: Math.max((scrollAmount += 500), scrollMax),
-      behavior: 'smooth',
-    });
+rightButton.addEventListener('click', () => {
+  index = Math.min(index + 1, n - 1);
+  cardContainer.scrollTo({
+    left: posLeft[index],
+    behavior: 'smooth',
   });
 });
